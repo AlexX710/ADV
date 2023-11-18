6bef08be1cffeb2023f403b2e2fb56fd8d1d2003
alexflores <afloresvaz710@gmail.com>
2023-11-17 19:03:36 -0600

    Chapter 2 modified, Chapter 5 added

--- Chapter 2.py
+++
@@ -1,9 +0,0 @@
-def chapter_2():
-    print("All of sudden you find yourself in a hedge maze")
-    choice = input("You are presented with 3 paths. Do you want to go left, right, or forward?").lower()
-    if choice == "Right":
-        print("Dead end, go back")
-        return True
-    else:
-        print("Choose a path")
-        return False
